c822742423be2cb08c716c89e17fb1ee940d9c69
zlydarv <104846873+Velfoy@users.noreply.github.com>
2022-10-03 21:57:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int Policz(int a, int b){
     return a * b;       //zamiana dodawania na mnożenie
 }
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     cout << "Na pierwszym branchu; 2 * 3 = " <<Policz( 2, 3) << endl;
     return 0;
 }
